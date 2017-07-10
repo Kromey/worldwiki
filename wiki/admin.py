118ea1c6c6a3409a4f984fcb4f74c0c274821d48
Travis Veazey <travisvz@gmail.com>
2017-07-10 15:26:47 -0800

    Add Page model to Admin site

--- wiki/admin.py
+++ wiki/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+
+from . import models
+
+
 # Register your models here.
+
+class PageAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(models.Page, PageAdmin)
--- wiki/models.py
+++ wiki/models.py
@@ -7,3 +7,6 @@ class Page(models.Model):
     path = models.CharField(max_length=255)
     body = models.TextField()
 
+    def __str__(self):
+        return '/' + self.path
+
