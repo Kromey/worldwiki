dc44a237bdc1f32dd8b7a379db41ef9905bc45cb
Travis Veazey <travisvz@gmail.com>
2017-07-10 16:48:28 -0800

    Parse Markdown into HTML

--- wiki/models.py
+++ wiki/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+from django.utils.safestring import mark_safe
+
+
+import bleach
+import markdown
 
 
 # Create your models here.
@@ -6,6 +11,14 @@ from django.db import models
 class Page(models.Model):
     path = models.CharField(max_length=255)
     body = models.TextField()
+
+    @property
+    def as_html(self):
+        clean_body = bleach.clean(self.body)
+        html = markdown.markdown(clean_body)
+        linked = bleach.linkify(html)
+
+        return mark_safe(linked)
 
     def __str__(self):
         return '/' + self.path
