ea1fd6112d8aa2680f2d0bbd28d1a70685f3ec15
Travis Veazey <travisvz@gmail.com>
2017-09-18 12:45:57 -0800

    Retrieve sidebar, or else hide it
    If there is an article with the slug "sidebar", it will be displayed as the sidebar on the page. Otherwise, the sidebar will be hidden from the page entirely and the content allowed to expand to the full width of the page.

--- wiki/context_processors.py
+++ wiki/context_processors.py
@@ -1,6 +1,18 @@
+from .models import Article
+
+
 def wiki_sidebar(request):
-    return {
-            'sidebar_content': 'This is the sidebar',
-            'sidebar_class': 'sidebar col-md-3',
-            'content_class': 'content col-md-9',
-            }
+    try:
+        sidebar = Article.objects.get(slug='sidebar')
+        return {
+                'sidebar_content': sidebar.html,
+                'sidebar_class': 'sidebar col-md-3',
+                'content_class': 'content col-md-9',
+                }
+    except Article.DoesNotExist:
+        return {
+                'sidebar_content': 'To display a sidebar, create an article with the slug "sidebar"',
+                'sidebar_class': 'sidebar hidden',
+                'content_class': 'content col-md-12',
+                }
+
