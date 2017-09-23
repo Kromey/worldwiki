c7aa8cc3d0f580ad13bb0c7da7df314ba315abc8
Travis Veazey <travisvz@gmail.com>
2017-09-22 16:10:55 -0800

    Switch to using Postgres for the backend

--- langthil/settings.py
+++ langthil/settings.py
@@ -79,8 +79,8 @@ WSGI_APPLICATION = 'langthil.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'langthil',
     }
 }
 
