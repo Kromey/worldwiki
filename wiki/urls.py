31eaf5e811216c22f3a0823f8380b948f2b10f10
Travis Veazey <travisvz@gmail.com>
2017-09-25 12:45:07 -0800

    New, Wikipedia-style slugify function

--- wiki/urls.py
+++ wiki/urls.py
@@ -6,7 +6,7 @@ from .views import ArticleListView,ArticleView,TagView
 
 urlpatterns = [
     url(r'^special:index$', ArticleListView.as_view(), name='wiki-index'),
-    url(r'^(?P<slug>[-\w]+)$', ArticleView.as_view(), name='wiki'),
-    url(r'^tag:(?P<slug>[-\w]+)$', TagView.as_view(), name='wiki-tag'),
+    url(r'^(?P<slug>[-\w_()]+)$', ArticleView.as_view(), name='wiki'),
+    url(r'^tag:(?P<slug>[-\w_()]+)$', TagView.as_view(), name='wiki-tag'),
     url(r'^$', ArticleView.as_view(), kwargs={'slug':'special:start'}, name='wiki-start'),
 ]
--- wiki/utils.py
+++ wiki/utils.py
@@ -1,18 +1,21 @@
 import re
 
 
-from django.template import defaultfilters
-
-
-SKIPPED_PREFIXES = [
+PREFIXES = [
         "a", "an", "as", "at", "before", "but", "by", "for", "from", "is",
         "in", "into", "like", "of", "off", "on", "onto", "per", "since",
         "than", "the", "this", "that", "to", "up", "via", "with"
         ];
-SKIPPED_RE = re.compile(r'^({pref})\b'.format(pref='|'.join(SKIPPED_PREFIXES)), re.I)
+PREFIX_RE = re.compile(r'^(\s*({pref})\b)+'.format(pref='|'.join(PREFIXES)), re.I)
+
+STRIP_RE = re.compile(r'[^-\w\s_()]')
+
+WHITESPACE_RE = re.compile(r'\s+')
 
 def slugify(text):
-    text = SKIPPED_RE.sub('', text)
+    text = PREFIX_RE.sub('', text)
+    text = STRIP_RE.sub('', text)
+    text = WHITESPACE_RE.sub('_', text.strip())
 
-    return defaultfilters.slugify(text)
+    return text
 
