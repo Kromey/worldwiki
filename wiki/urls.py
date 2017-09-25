b52ce89d8e87f15a9fe8e6471c6b5f12cd92a558
Travis Veazey <travisvz@gmail.com>
2017-09-25 14:48:22 -0800

    Rewrite wiki page logic as View subclass
    We were overriding so much of DetailView, with more to come, that it makes more sense to go to a View subclass instead

--- wiki/urls.py
+++ wiki/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import url
 
 
-from .views import ArticleListView,ArticleView,TagView
+from .views import ArticleListView,TagView,WikiPageView
 
 
 urlpatterns = [
     url(r'^special:index$', ArticleListView.as_view(), name='wiki-index'),
-    url(r'^(?P<slug>[-\w_()]+)$', ArticleView.as_view(), name='wiki'),
+    url(r'^(?P<slug>[-\w_()]+)$', WikiPageView.as_view(), name='wiki'),
     url(r'^tag:(?P<slug>[-\w_()]+)$', TagView.as_view(), name='wiki-tag'),
-    url(r'^$', ArticleView.as_view(), kwargs={'slug':'special:start'}, name='wiki-start'),
+    url(r'^$', WikiPageView.as_view(), kwargs={'slug':'special:start'}, name='wiki-start'),
 ]
--- wiki/views.py
+++ wiki/views.py
@@ -1,5 +1,6 @@
 from django.http import Http404
 from django.shortcuts import render
+from django.views import View
 from django.views.generic import DetailView,ListView
 
 
@@ -25,38 +26,18 @@ class ArticleListView(ListView):
     context_object_name = 'articles'
 
 
-class ArticleView(DetailView):
-    context_object_name = 'article'
+class WikiPageView(View):
+    article_template = 'wiki/article_detail.html'
+    nsfw_template = 'wiki/article_nsfw.html'
     show_nsfw_content = False
-    nsfw_template_name = 'wiki/article_nsfw.html'
 
-    def get_queryset(self):
-        if self.request.user.has_perm('wiki.change_article'):
-            return Article.objects.all()
-        else:
-            return Article.objects.filter(is_published=True)
-
-    def get_object(self, queryset=None):
-        try:
-            return super().get_object(queryset)
-        except Http404 as e:
-            try:
-                return Article.objects.get(slug='special:404')
-            except Article.DoesNotExist:
-                raise e
-
-    def get_template_names(self):
-        names = super().get_template_names()
-
-        if self.object.is_nsfw and not self.show_nsfw_content:
-            names.insert(0, self.nsfw_template_name)
-
-        return names
-
-    def get(self, request, *args, **kwargs):
+    def get(self, request, slug):
         self.show_nsfw_content = self.show_nsfw_content or request.session.get('show_nsfw', False)
 
-        return super().get(request, *args, **kwargs)
+        try:
+            return self.get_article(request, slug)
+        except Article.DoesNotExist:
+            return self.get_article(request, 'special:404')
 
     def post(self, request, *args, **kwargs):
         if request.POST.get('show-me'):
@@ -65,4 +46,17 @@ class ArticleView(DetailView):
                 request.session['show_nsfw'] = True
 
         return self.get(request, *args, **kwargs)
+
+    def get_article(self, request, slug):
+        if self.request.user.has_perm('wiki.change_article'):
+            qs = Article.objects
+        else:
+            qs = Article.objects.filter(is_published=True)
+
+        article = qs.get(slug=slug)
+
+        if article.is_nsfw and not self.show_nsfw_content:
+            return render(request, self.nsfw_template, context={'article':article})
+        else:
+            return render(request, self.article_template, context={'article':article})
 
