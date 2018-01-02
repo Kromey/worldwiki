d97ae9042a6fe3fb5ca26847193438cfd944693b
Travis Veazey <travisvz@gmail.com>
2018-01-02 14:03:15 -0900

    Add : to wiki-edit URL pattern

--- wiki/urls.py
+++ wiki/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     url(r'^special:preview$', PreviewView.as_view(), name='wiki-preview'),
 
     url(r'^(?P<slug>[-\w_()]+)$', WikiPageView.as_view(), name='wiki'),
-    url(r'^(?P<slug>[-\w_()]+)/edit$', WikiEditView.as_view(), name='wiki-edit'),
+    url(r'^(?P<slug>[-\w:_()]+)/edit$', WikiEditView.as_view(), name='wiki-edit'),
     url(r'^(?P<slug>[-\w_()]+)/new$', WikiEditView.as_view(), name='wiki-new'),
 
     url(r'^tag:(?P<slug>[-\w_()]+)$', TagView.as_view(), name='wiki-tag'),
