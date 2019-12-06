e7a1419f065b85bdb4e69a366e8494a805f681eb
Travis Veazey <travisvz@gmail.com>
2019-12-06 11:06:51 -0900

    Renaming the project
    Making this a generic, worldbuilding-focused wiki, rather than one just for a single world.

--- worldwiki/wsgi.py
+++ worldwiki/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for langthil project.
+WSGI config for WorldWiki project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "langthil.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "worldwiki.settings")
 
 application = get_wsgi_application()
