75e943042fc07e6759a892e03e5afe7cb7a79920
Travis Veazey <travisvz@gmail.com>
2019-12-19 13:46:08 -0900

    Use pages module to find sidebar

--- wiki/context_processors.py
+++ wiki/context_processors.py
@@ -1,25 +1,25 @@
 from django.conf import settings
 
 
-from .models import Article
+from wiki.models import Article
+from wiki.pages import WikiSidebar
 
 
-class WikiSidebar:
-    SLUG = 'sidebar'
+class Sidebar:
     def __init__(self):
         try:
-            sidebar = Article.objects.get(slug=WikiSidebar.SLUG)
+            sidebar = Article.objects.get(**WikiSidebar)
 
             self.content = sidebar.html
             self.css = 'sidebar col-md-3'
             self.exists = True
         except Article.DoesNotExist:
-            self.content = 'To display a sidebar, create an article with the slug "{slug}"'.format(slug=WikiSidebar.SLUG)
+            self.content = 'To display a sidebar, create an article with the slug "{slug}"'.format(slug=WikiSidebar['slug'])
             self.css = 'sidebar hidden'
             self.exists = False
 
 def wiki(request):
-    sidebar = WikiSidebar()
+    sidebar = Sidebar()
 
     content_css = 'content col-md-{}'.format('9' if sidebar.exists else '12')
 
