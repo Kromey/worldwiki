0c77739378e20458d13f2b79d38222d2e03129bc
Travis Veazey <travisvz@gmail.com>
2020-01-17 13:20:36 -0900

    Updating edit/create/move URLs
    Updating the relevant URLs to a format that can't conflict with a valid
    wiki path; the former approach was incompatible with e.g. a page with
    slug 'edit' under any namespace but the root, as there was no difference
    between the path `<namespace>/edit` and the URL `<path>/edit`.

--- wiki/urls.py
+++ wiki/urls.py
@@ -33,18 +33,18 @@ register_converter(WikiSlugConverter, 'wikislug')
 
 urlpatterns = [
     path('glossary', views.GlossaryView.as_view(), name='wiki-glossary'),
-    path('glossary/edit', views.TermCreateView.as_view(), name='wiki-term-new'),
-    path('glossary/edit/<int:pk>', views.TermEditView.as_view(), name='wiki-term-edit'),
+    path('glossary;action=edit', views.TermCreateView.as_view(), name='wiki-term-new'),
+    path('glossary;action=edit&id=<int:pk>', views.TermEditView.as_view(), name='wiki-term-edit'),
 
     path('special:index', views.ArticleListView.as_view(), name='wiki-index'),
     path('special:preview', views.PreviewView.as_view(), name='wiki-preview'),
 
     path('tag:<wikislug:slug>', views.TagView.as_view(), name='wiki-tag'),
-    path('tag:<wikislug:slug>/edit', views.TagEditView.as_view(), name='wiki-tag-edit'),
+    path('tag:<wikislug:slug>;action=edit', views.TagEditView.as_view(), name='wiki-tag-edit'),
 
-    path('<namespace:namespace><wikislug:slug>/new', views.WikiCreateView.as_view(), name='wiki-new'),
-    path('<namespace:namespace><wikislug:slug>/edit', views.WikiUpdateView.as_view(), name='wiki-edit'),
-    path('<namespace:namespace><wikislug:slug>/move', views.WikiMoveView.as_view(), name='wiki-move'),
+    path('<namespace:namespace><wikislug:slug>;action=create', views.WikiCreateView.as_view(), name='wiki-new'),
+    path('<namespace:namespace><wikislug:slug>;action=edit', views.WikiUpdateView.as_view(), name='wiki-edit'),
+    path('<namespace:namespace><wikislug:slug>;action=move', views.WikiMoveView.as_view(), name='wiki-move'),
     path('<namespace:namespace><wikislug:slug>', views.WikiPageView.as_view(), name='wiki'),
     path('', views.WikiPageView.as_view(), kwargs=WikiStart, name='wiki-start'),
 ]
