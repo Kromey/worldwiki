4a1b36a374cf3f822900f72dd31a5c8270375cdb
Travis Veazey <travisvz@gmail.com>
2020-02-18 11:17:52 -0900

    Use bleach to sanitize HTML
    This closes a potential security issue from the use of the
    attribute_lists Markdown extension, as well as opens the door for us to
    include "fancier" markup in the output, such as when we begin to add
    macros.
    
    refs #3

--- wiki/markdown/markdown.py
+++ wiki/markdown/markdown.py
@@ -1,7 +1,11 @@
 import bleach
+from bleach_whitelist import markdown_tags,markdown_attrs
 import markdown
 from markdown.extensions.toc import TocExtension
 
+
+markdown_tags.append('del')
+markdown_attrs['*'].append('class')
 
 class Markdown:
     __converter = markdown.Markdown(
@@ -15,7 +19,6 @@ class Markdown:
             'wiki.markdown.extensions.strikethrough',
             'wiki.markdown.extensions.tbd',
             'wiki.markdown.extensions.wikilinks',
-            'wiki.markdown.extensions.escapehtml',
             ],
         )
     __linker = bleach.linkifier.Linker(callbacks=[])
@@ -25,18 +28,30 @@ class Markdown:
         if not meta:
             meta = {}
 
+        # Set up our metadata
         cls.__converter.reset()
         cls.__converter.Meta = meta
+        # Convert the Markdown to HTML
         html = cls.__converter.convert(md)
+
+        # Sanitize the HTML
+        # Don't use a global class instance, as it's not thread-safe
+        html = bleach.clean(html, markdown_tags, markdown_attrs)
+        # Linkify any additional URLs in the text
         html = cls.__linker.linkify(html)
 
+        # If we have at least 3 headers and no explicit TOC token, add the TOC
         if not '[TOC]' in md and cls.__converter.toc.count('<li>') >= 3:
             lines = html.splitlines()
             for i in range(len(lines)):
+                # Look for the first header
+                # We know we'll find one because there's at least 3
                 if lines[i].startswith('<h'):
                     break
 
+            # Insert the TOC before the first header
             lines.insert(i, cls.__converter.toc)
+            # Recombine the lines into a single document
             html = '\n'.join(lines)
 
         return html
